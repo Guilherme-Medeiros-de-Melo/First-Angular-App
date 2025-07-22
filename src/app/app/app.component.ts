5f94b9455fd5fa4e47ce56e38d0f07f57aa2f90b
GuilhermeMM <114604987+Guilherme-Medeiros-de-Melo@users.noreply.github.com>
2025-07-22 15:19:41 -0300

    Added For If examples for Angular 17 and older

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -5,16 +5,19 @@ import { UserComponent } from '../user/user.component';
 import { DUMMY_USERS } from '../user/dummy-users';
 import { TaskComponent } from "../task/task.component";
 
+//Loops for Angular 17 and earlier
+import { NgFor, NgIf } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 1;
+  selectedUserId?:number;
 
   get selectedUser(){
     return this.users.find((user) => user.id === this.selectedUserId);
