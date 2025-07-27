910db43bafd8c7afbe138ac468d4184370ac8857
GuilhermeMM <114604987+Guilherme-Medeiros-de-Melo@users.noreply.github.com>
2025-07-27 12:03:35 -0300

    Added Directive and TWB to new-task

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,19 @@
 import { Component, output, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   cancel = output<void>()
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
   
   onCancel(){
     this.cancel.emit();
